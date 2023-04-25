375fb5864fc97d2164a1ccd2be94fcf6a21e3dcf
ABHINAND A S <abhinandunnikuttan04@gmail.com>
2023-04-26 04:30:53 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1650,7 +1650,7 @@ async def advantage_spell_chok(client, msg):
                  await spell_check_del.delete() 
   
   
- async def manual_filters(client, message, text=False):
+async def manual_filters(client, message, text=False):
 settings = await get_settings(message.chat.id)
 group_id = message.chat.id
 name = text or message.text
