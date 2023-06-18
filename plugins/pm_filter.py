786752b411ddc0d1d8047701721d5b30d4b2856b
ABHINAND A S <abhinandunnikuttan04@gmail.com>
2023-06-18 09:55:09 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -297,17 +297,17 @@ async def language_check(bot, query):
      files, offset, total_results = await get_search_results(query.message.chat.id, movie, offset=0, filter=True)
      if files:
          settings = await get_settings(query.message.chat.id)
-        temp.SEND_ALL_TEMP[query.from_user.id] = files
-        if 'is_shortlink' in settings.keys():
-            ENABLE_SHORTLINK = settings['is_shortlink']
-        else:
-            await save_group_settings(query.message.chat.id, 'is_shortlink', False)
-            ENABLE_SHORTLINK = False
-        pre = 'filep' if settings['file_secure'] else 'file'
-        if ENABLE_SHORTLINK and settings['button']:
-            btn = [
-                [
-                    InlineKeyboardButton(
+         temp.SEND_ALL_TEMP[query.from_user.id] = files
+         if 'is_shortlink' in settings.keys():
+             ENABLE_SHORTLINK = settings['is_shortlink']
+         else:
+             await save_group_settings(query.message.chat.id, 'is_shortlink', False)
+             ENABLE_SHORTLINK = False
+         pre = 'filep' if settings['file_secure'] else 'file'
+         if ENABLE_SHORTLINK and settings['button']:
+             btn = [
+                 [
+                     InlineKeyboardButton(
                         text=f"[{get_size(file.file_size)}] {file.file_name}", url=await get_shortlink(query.message.chat.id, f"https://telegram.me/{temp.U_NAME}?start=files_{file.file_id}")
                     ),
                 ]
